3502650fac2808bf95499d0b2f95eef9ab5d5f85
bys-exe <batchuyugasai@gmail.com>
2026-05-03 06:18:57 +0000

    Refactor terminal handling and add ~
    added ~ to each row to show that the line isnt being used, next commit will change the no of ~ acc to screen size

--- scribere.c
+++ scribere.c
@@ -7,46 +7,97 @@
 #include <termios.h>
 #include <unistd.h>
 
+// defined
+
+#define CTRL_KEY(k) ((k) & 0x1f) // ctrl key shorthand
+
 // data
 
 struct termios orig_termios;
 
 // terminal
 
-void die(const char *s){
+void die(const char *s) // display error message
+{
+    write(STDIN_FILENO, "\x1b[2J", 4); // clear screen
+    write(STDIN_FILENO, "\x1b[H", 3);  // starts the cursor from top left corner
     perror(s);
     exit(1);
 }
-void disableRawMode(){
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+void disableRawMode() // default mode terminal is on
+{
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
-void enableRawMode(){
-    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
-    atexit(disableRawMode);
+void enableRawMode() // gives complete control over terminal screen
+{
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
+    atexit(disableRawMode); // runs this right before code is exited
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(BRKINT | ICRNL | IXON | INPCK | ISTRIP);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag &= ~(CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_iflag &= ~(BRKINT | ICRNL | IXON | INPCK | ISTRIP); // disable input flags
+    raw.c_oflag &= ~(OPOST);                                  // disable output flags
+    raw.c_cflag &= ~(CS8);                                    // disable control flags
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);          // disable local flags
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
+}
+char editorReadKey() // read the keypress from user
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
 }
 
-// init (this happens first after boot)
+// output
+
+void editorDrawRows()
+{ // display ~ to indicate lines which arent being used
+    int y;
+    for (y = 0; y < 24; y++)
+    {
+        write(STDIN_FILENO, "~\r\n", 3);
+    }
+}
+void editorRefreshScreen() // clearing the screen (2 represents clearing the whole screen in below line)
+{
+    write(STDIN_FILENO, "\x1b[2J", 4); // \x1b is escape character (1 byte), [ 2 J are 1 byte each respectively
+    write(STDIN_FILENO, "\x1b[H", 3);  // starts the cursor from top left corner
+    editorDrawRows();
+    write(STDIN_FILENO, "\x1b[H", 3);
+}
+
+// input
+
+void editorProcessKeypress() // reads the keypress then handles it
+{
+    char c = editorReadKey();
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        write(STDIN_FILENO, "\x1b[2J", 4); // clear screen
+        write(STDIN_FILENO, "\x1b[H", 3);  // starts the cursor from top left corner
+        exit(0);
+        break;
+    }
+}
+
+// init (this happens first after boot/launching)
 
 int main()
 {
     enableRawMode();
-    while (1){
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if(iscntrl(c)){
-            printf("%d\r\n",c);
-        }else{
-            printf("%d ('%c')\r\n",c,c);
-        }
-        if(c == 'q')break;
+    while (1)
+    {
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
