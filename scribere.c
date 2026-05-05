3656febcf35ec326c83dba4abd36a66216fd0006
bys-exe <batchuyugasai@gmail.com>
2026-05-05 13:59:57 +0000

    Implement arrow keys for navigation

--- scribere.c
+++ scribere.c
@@ -13,11 +13,19 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f) // ctrl key shorthand
 #define SCRIBERE_VERSION "0.0.1"
+enum editorKey
+{
+    ARROW_LEFT = 1000, // gave it a huge value which wont conflict ordinary keypresses
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
 
 // data
 
 struct editorConfig
 {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -30,17 +38,17 @@ void die(const char *s) // display error message
 {
     write(STDIN_FILENO, "\x1b[2J", 4); // clear screen
     write(STDIN_FILENO, "\x1b[H", 3);  // starts the cursor from top left corner
-    perror(s);
+    perror(s);                         // display where we got error
     exit(1);
 }
 void disableRawMode() // default mode terminal is on
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) // error handling
         die("tcsetattr");
 }
 void enableRawMode() // gives complete control over terminal screen
 {
-    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) // error handling
         die("tcgetattr");
     atexit(disableRawMode); // runs this right before code is exited
     struct termios raw = E.orig_termios;
@@ -50,10 +58,10 @@ void enableRawMode() // gives complete control over terminal screen
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);          // disable local flags
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) // error handling
         die("tcsetattr");
 }
-char editorReadKey() // read the keypress from user
+int editorReadKey() // read the keypress from user
 {
     int nread;
     char c;
@@ -61,6 +69,33 @@ char editorReadKey() // read the keypress from user
     {
         if (nread == -1 && errno != EAGAIN)
             die("read");
+    }
+    if (c == '\x1b')
+    {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+        if (seq[0] == '[')
+        {
+            switch (seq[1])
+            {
+            case 'A': // up arrow key
+                return ARROW_UP;
+            case 'B': // down arrow key
+                return ARROW_DOWN;
+            case 'C': // right arrow key
+                return ARROW_RIGHT;
+            case 'D': // left arrow key
+                return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    }
+    else
+    {
+        return c;
     }
     return c;
 }
@@ -110,7 +145,9 @@ int getWindowSize(int *rows, int *cols) // gets windows size
 }
 void initEditor()
 {
-    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+    E.cx = 0;                                              // row pos is o
+    E.cy = 0;                                              // col pos is 0
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) // error handling
         die("getWindowSize");
 }
 
@@ -174,10 +211,12 @@ void editorDrawRows(struct abuf *ab) // display ~ on each line to indicate lines
 void editorRefreshScreen() // clearing the screen (2 represents clearing the whole screen in below line)
 {
     struct abuf ab = ABUF_INIT;
-    abAppend(&ab, "\x1b[?25l", 6); // hide cursor before refreshing (l is hide cursor)
+    abAppend(&ab, "\x1b[?25l", 6); // hide cursor before refreshing (l is hide cursor) (cursor might flicker thats why)
     abAppend(&ab, "\x1b[H", 3);    // starts the cursor from top left corner
     editorDrawRows(&ab);
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1); // move cursor to position in cx and cy
+    abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6); // show cursor since refreshing is done (h is show cursor)
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
@@ -185,15 +224,53 @@ void editorRefreshScreen() // clearing the screen (2 represents clearing the who
 
 // input
 
+void editorMoveCursor(int key)
+{
+    switch (key)
+    {
+    case ARROW_LEFT:
+        if (E.cx != 0)
+        {
+            E.cx--;
+        }
+        break;
+    case ARROW_RIGHT:
+        if (E.cx != E.screencols - 1)
+        {
+            E.cx++;
+        }
+        break;
+    case ARROW_UP:
+        if (E.cy != 0)
+        {
+            E.cy--;
+        }
+        break;
+    case ARROW_DOWN:
+        if (E.cy != E.screenrows - 1)
+        {
+            E.cy++;
+        }
+        break;
+    }
+}
+
 void editorProcessKeypress() // reads the keypress then handles it
 {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c)
     {
     case CTRL_KEY('q'):
         write(STDIN_FILENO, "\x1b[2J", 4); // clear screen
         write(STDIN_FILENO, "\x1b[H", 3);  // starts the cursor from top left corner
         exit(0);
+        break;
+
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+        editorMoveCursor(c);
         break;
     }
 }
