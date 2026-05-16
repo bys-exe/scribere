20b683a52cf68d00ee1bcda0e4a6866ba02fbc1a
bys-exe <batchuyugasai@gmail.com>
2026-05-16 10:36:04 +0000

    Handle tab spaces

--- scribere.c
+++ scribere.c
@@ -18,6 +18,7 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f) // ctrl key shorthand
 #define SCRIBERE_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 enum editorKey
 {
     ARROW_LEFT = 1000, // gave it a huge value which wont conflict ordinary keypresses
@@ -36,12 +37,15 @@ enum editorKey
 typedef struct erow // editor row
 {
     int size;
+    int rsize;
     char *chars;
+    char *render; // prevents extra spaces in terminal
 } erow;
 
 struct editorConfig
 {
     int cx, cy;
+    int rx; // handling tab spaces
     int rowoff;
     int coloff;
     int screenrows;
@@ -208,6 +212,48 @@ int getWindowSize(int *rows, int *cols) // gets windows size
 
 // row operations
 
+int editorRowCxToRx(erow *row, int cx)
+{
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++)
+    {
+        if (row->chars[j] == '\t')
+            rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+        rx++;
+    }
+    return rx;
+}
+
+void editorUpdateRow(erow *row)
+{
+    int tabs = 0;
+    int j;
+    for (j = 0; j < row->size; j++)
+    {
+        if (row->chars[j] == '\t')
+            tabs++;
+        free(row->render);                                                // prevents memory leak (frees old memory)
+        row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1); // +1 for the '\0' operator
+    }
+    int idx = 0;
+    for (j = 0; j < row->size; j++)
+    {
+        if (row->chars[j] == '\t')
+        {
+            row->render[idx++] = ' ';
+            while (idx % KILO_TAB_STOP != 0)
+                row->render[idx++] = ' ';
+        }
+        else
+        {
+            row->render[idx++] = row->chars[j]; // copies each char from raw file into render (display)
+        }
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) // lets us read multiple lines
 {
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
@@ -216,6 +262,9 @@ void editorAppendRow(char *s, size_t len) // lets us read multiple lines
     E.row[at].chars = malloc(len + 1); // +1 to hold \0
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
     E.numrows++; // to indicate erow now contains a line that should be displayed
 }
 
@@ -266,6 +315,11 @@ void abFree(struct abuf *ab) // it deallocates the dynamic memory used by abuf
 
 void editorScroll()
 {
+    E.rx = 0;
+    if (E.cy < E.numrows)
+    {
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+    }
     if (E.cy < E.rowoff)
     {
         E.rowoff = E.cy;
@@ -274,13 +328,13 @@ void editorScroll()
     {
         E.rowoff = E.cy - E.screenrows + 1;
     }
-    if (E.cx < E.coloff)
+    if (E.rx < E.coloff)
     {
-        E.coloff = E.cx;
+        E.coloff = E.rx;
     }
-    if (E.cx >= E.coloff + E.screencols)
+    if (E.rx >= E.coloff + E.screencols)
     {
-        E.coloff = E.cx - E.screencols + 1;
+        E.coloff = E.rx - E.screencols + 1;
     }
 }
 
@@ -315,12 +369,12 @@ void editorDrawRows(struct abuf *ab) // display ~ on each line to indicate lines
         }
         else
         {
-            int len = E.row[filerow].size - E.coloff;
+            int len = E.row[filerow].rsize - E.coloff;
             if (len < 0)
                 len = 0;
             if (len > E.screencols)
                 len = E.screencols;
-            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+            abAppend(ab, &E.row[filerow].render[E.coloff], len);
         }
         abAppend(ab, "\x1b[K", 3); // erases part of line
         if (y < E.screenrows - 1)
@@ -337,7 +391,7 @@ void editorRefreshScreen() // clearing the screen (2 represents clearing the who
     abAppend(&ab, "\x1b[H", 3);    // starts the cursor from top left corner
     editorDrawRows(&ab);
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1); // move cursor to position in cx and cy
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1); // move cursor to position in cx and cy
     abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6); // show cursor since refreshing is done (h is show cursor)
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -348,6 +402,7 @@ void editorRefreshScreen() // clearing the screen (2 represents clearing the who
 
 void editorMoveCursor(int key)
 {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
     switch (key)
     {
     case ARROW_LEFT:
@@ -355,9 +410,22 @@ void editorMoveCursor(int key)
         {
             E.cx--;
         }
+        else if (E.cy > 0) // go to previous line when press <- at start of line
+        {
+            E.cy--;
+            E.cx = E.row[E.cy].size;
+        }
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (row && E.cx < row->size)
+        {
+            E.cx++;
+        }
+        else if (row && E.cx == row->size) // go to next line when press -> at end of line
+        {
+            E.cy++;
+            E.cx = 0;
+        }
         break;
     case ARROW_UP:
         if (E.cy != 0)
@@ -371,6 +439,12 @@ void editorMoveCursor(int key)
             E.cy++;
         }
         break;
+    }
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen)
+    {
+        E.cx = rowlen;
     }
 }
 
@@ -413,6 +487,7 @@ void initEditor()
 {
     E.cx = 0;     // row pos is 0 at start
     E.cy = 0;     // col pos is 0 at start
+    E.rx = 0;     // used for handling tab spaces
     E.rowoff = 0; // scrolled to top at start
     E.coloff = 0;
     E.numrows = 0;
