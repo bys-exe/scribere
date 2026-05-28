cf264fdaab7fdda718a792391419d2f5f808d8a2
bys-exe <batchuyugasai@gmail.com>
2026-05-28 13:02:42 +0000

    Highlight comments & strings

--- scribere.c
+++ scribere.c
@@ -39,10 +39,13 @@ enum editorKey
 enum editorHighlight
 {
     HL_NORMAL = 0,
+    HL_COMMENT,
+    HL_STRING,
     HL_NUMBER,
     HL_MATCH
 };
 #define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
 
 // data
 
@@ -50,6 +53,7 @@ struct editorSyntax
 {
     char *filetype;
     char **filematch;
+    char *singleline_comment_start;
     int flags;
 };
 typedef struct erow // editor row
@@ -86,8 +90,10 @@ char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 struct editorSyntax HLDB[] = {
     {"c",
      C_HL_extensions,
-     HL_HIGHLIGHT_NUMBERS},
+     "//",
+     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
 };
+
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
 
 // prototypes (just to let the compiler know the function exists later to stop throwing errors)
@@ -262,12 +268,51 @@ void editorUpdateSyntax(erow *row)
     memset(row->hl, HL_NORMAL, row->rsize);
     if (E.syntax == NULL)
         return;
+    char *scs = E.syntax->singleline_comment_start;
+    int scs_len = scs ? strlen(scs) : 0;
     int prev_sep = 1;
+    int in_string = 0;
     int i = 0;
     while (i < row->rsize)
     {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+        if (scs_len && !in_string)
+        {
+            if (!strncmp(&row->render[i], scs, scs_len))
+            {
+                memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+                break;
+            }
+        }
+        if (E.syntax->flags & HL_HIGHLIGHT_STRINGS)
+        {
+            if (in_string)
+            {
+                row->hl[i] = HL_STRING;
+                if (c == '\\' && i + 1 < row->rsize)
+                {
+                    row->hl[i + 1] = HL_STRING;
+                    i += 2;
+                    continue;
+                }
+                if (c == in_string)
+                    in_string = 0;
+                i++;
+                prev_sep = 1;
+                continue;
+            }
+            else
+            {
+                if (c == '"' || c == '\'')
+                {
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                    i++;
+                    continue;
+                }
+            }
+        }
         if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS)
         {
             if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER))
@@ -286,10 +331,14 @@ int editorSyntaxToColor(int hl)
 {
     switch (hl)
     {
+    case HL_COMMENT:
+        return 36; // cyan
+    case HL_STRING:
+        return 32; // green
     case HL_NUMBER:
         return 31; // red
     case HL_MATCH:
-        return 34;
+        return 34; // blue
     default:
         return 37; // white
     }
